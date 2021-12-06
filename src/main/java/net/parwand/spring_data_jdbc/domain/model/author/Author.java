3ef1d8ef1aa6482fcdde63ba115b3109bec80883
Parwand <parwand.alsino@hhu.de>
2021-12-06 13:06:21 +0100

    Getter and Setter in Author

--- src/main/java/net/parwand/spring_data_jdbc/domain/model/author/Author.java
+++ src/main/java/net/parwand/spring_data_jdbc/domain/model/author/Author.java
@@ -26,6 +26,18 @@ public class Author {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
